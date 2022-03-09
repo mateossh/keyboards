2a06d6c8c0731bfe394a193fb986cc86b86455dc
Mateusz Ż <zochowski.mateusz@outlook.com>
2022-03-09 09:56:41 +0100

    planck: update keycodes to swap only lgui with lalt

--- planck/keymap.c
+++ planck/keymap.c
@@ -43,6 +43,9 @@ enum planck_keycodes {
 #define RAISE MO(_RAISE)
 #define ESC_MOD LT(_UTILS, KC_ESC) //        KC_GESC <- KC_ESC
 
+#define LT_SWAP MAGIC_SWAP_LALT_LGUI
+#define LT_UNSWAP MAGIC_UNSWAP_LALT_LGUI
+
 // Tap once: [. Hold: LALT. Tap twice: {
 #define KC_TLALT TD(TD_LBRC_LALT_LCBR)
 // Tap once: ]. Hold: RALT. Tap twice: }
@@ -137,7 +140,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_ADJUST] = LAYOUT_planck_grid(
     _______, RESET,   DEBUG,   RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD,  RGB_VAI, RGB_VAD, KC_DEL ,
-    _______, _______, MU_MOD,  AU_ON,   AU_OFF,  AG_NORM, AG_SWAP, QWERTY,  COLEMAK,  _______, _______, _______,
+    _______, _______, MU_MOD,  AU_ON,   AU_OFF,  LT_SWAP, LT_UNSWAP, QWERTY,  COLEMAK,  _______, _______, _______,
     _______, MUV_DE,  MUV_IN,  MU_ON,   MU_OFF,  MI_ON,   MI_OFF,  TERM_ON, TERM_OFF, _______, _______, _______,
     _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______, _______, _______
 ),
